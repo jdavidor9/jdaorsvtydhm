4bacaf83dcb0a66533e90a41eda219d004202ab3
Jesus David Olivera Rivera <joliverar@choucairtesting.com>
2024-05-09 10:07:46 -0500

    hola mundo msj

---
+++ jdaor.py
@@ -0,0 +1,23 @@
+#00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+
+#https://www.python.org/
+
+"""
+Este es un comentario
+de varias líneas en Python.
+esto puede ocupar más de una línea.
+"""
+
+
+variable = "variable"
+variable = 1
+CONSTANTE = "constante"
+CNSTANTE = 10
+
+entero_int = 3
+decimal_float = 10.0
+true_bool = True
+true_bool = False
+texto_str = "cadena de texto"
+
+print("Hola python")
